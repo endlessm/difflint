e9706c544221466ac46b5893bf6289f51a058e06
Devin Ekins <devinj.ekins@gmail.com>
2014-12-19 11:12:32 -0700

    Added JS linting config files.
    The lint.sh script assumes the configuration files are in the
    home directory of the user. It should eventually only use those
    as a fallback, but first look for configuration files locally.
    
    Non-default fallback to NodeJS can be specified via a command line
    argument of "node" (without quotes) following the file to be linted.
    Alternatively, regular Javascript (or web javascript) can be linted
    for via the "web" argument.
    
    Example: ./lint.sh my_file.js node
    
    Example: ./lint.sh my_file.js web

---
+++ lint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file_to_lint="$1"
+mode="$2"
+
+if [ "$mode" == "node" -o "$mode" == "nodejs" ]
+  then
+    echo "Linting assuming node.js"
+    jscs "$file_to_lint"
+    jshint "$file_to_lint" --config="/usr/share/node.jshintrc"
+elif [ "$mode" == "web" -o "$mode" == "webjs" ]
+  then
+    echo "Linting assuming web (normal) js"
+    jscs "$file_to_lint"
+    jshint "$file_to_lint" --config="/usr/share/web.jshintrc"
+else
+    echo "Linting based on closest configuration file"
+    jscs "$file_to_lint"
+    jshint "$file_to_lint"
+fi
