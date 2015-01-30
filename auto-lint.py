0fafc7f74c5af6af32c321773016f072c8bce734
Devin Ekins <devinj.ekins@gmail.com>
2015-01-30 16:25:07 -0700

    Linting diffs used in pre-commit hook.
    When a file is modified relative to a previous git revision,
    the pre-commit hook (currently titled auto-lint.py) will compare
    the difference between the linting outputs of the file in the
    previous commit versus the file you are currently attempting to
    commit. This difference will be noted and logged to a file.
    
    If the linting output is different it probably either
    indicates more errors or fewer. If more errors are produced,
    this is a sign that the commit should be amended to address them.
    If fewer are produced, that is a good thing. A followup issue
    will address the correction of previous errors/warnings.
    
    This issue requires that the lint.sh used does not exit with
    status code 2 (which another issue addresses) as that would
    prevent the commit from succeeding. This currently happens when
    a JSHint check fails to pass.

--- auto-lint.py
+++ auto-lint.py
@@ -1,31 +1,108 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from os import getcwd, listdir
+from datetime import datetime
+from difflib import unified_diff
+from io import StringIO
+from os import getcwd, listdir, unlink
 from os.path import exists, isfile, join, splitext
 from subprocess import call, check_output
 from sys import stderr
 
+LOG_FILE = "lintdiff.log"
+LINT_PATH = "/usr/bin/lint.sh"
+
+def lint_list(file_list):
+    '''
+    Lints every file within file list with a linter
+    appropriate to its extension. If no linter exists
+    for that file type, this function will ignore that
+    file.
+
+    Input: A list of filenames containing no duplicates.
+
+    Returns: A mapping of filenames to their linted
+             output.
+    '''
+    # Mapping of {filename : linting output}
+    lint_mapping = {}
+
+    for f in file_list:
+        root, ext = splitext(f)
+        if ext == ".js":
+            lint_output = check_output(["/usr/bin/lint.sh", f])
+            lint_mapping[f] = lint_output.decode()
+        # TODO: Other file extensions/linters will be added here.
+
+    return lint_mapping
 
 def main():
-    lint_path = "/usr/bin/lint.sh"
-    if not exists(lint_path):
-        stderr.write("No lint script found at '" + lint_path + "'!\n")
+    if not exists(LINT_PATH):
+        stderr.write("No lint script found at '" + LINT_PATH + "'!\n")
         return
 
+    # Put all changes made that *are not* being committed in the stash.
+    call(["git", "stash", "save", "--keep-index", "--quiet",
+          '"pre-commit hook unstaged changes"'])
+
     changed_file_output = check_output(["git", "diff", "--name-only",
-                                        "--staged", "--diff-filter=ABCM"])
-    changed_file_list = map(bytes.decode, changed_file_output.split())
-    for f in changed_file_list:
-        root, ext = splitext(f)
-        if ext == ".py":
-            print(f + " is a Python file.")
-        elif ext == ".c":
-            print(f + " is a C file.")
-        elif ext == ".h":
-            print(f + " is a C header file.")
-        elif ext == ".js":
-            print(f + " is a JavaScript file.")
-            call(["/usr/bin/lint.sh", f])
+                                        "--staged", "--diff-filter=CM"])
+    changed_file_list = list(map(bytes.decode, changed_file_output.split()))
+    current_lint_mapping = lint_list(changed_file_list)
+
+    # Now, we'll roll back changes that *are* being committed as well to
+    # get the baseline linting output.
+    call(["git", "stash", "save", "--quiet",
+          '"pre-commit hook staged changes"'])
+
+    past_lint_mapping = lint_list(changed_file_list)
+
+    # Restore the changes that WILL NOT be committed first.
+    call(["git", "stash", "apply", "--index", "--quiet", "stash@{1}"])
+
+    # Restore the changes that WILL be committed now.
+    call(["git", "stash", "apply", "--index", "--quiet", "stash@{0}"])
+
+    # Remove both stash frames.
+    call(["git", "stash", "drop", "--quiet"])
+    call(["git", "stash", "drop", "--quiet"])
+
+    # Compare the two linting output dictionaries.
+    any_errors_introduced = False
+    diff_output = StringIO()
+    for filename, output in current_lint_mapping.items():
+        if current_lint_mapping[filename] == past_lint_mapping[filename]:
+            print("Changes to " + filename + " introduced no new linting " +
+                  "errors.")
+            continue
+        # If this is the first error, start the LOG_FILE with a timestamp.
+        if not any_errors_introduced:
+            any_errors_introduced = True
+            diff_output.write(str(datetime.utcnow().isoformat(" ")) + "\n")
+                
+        print("WARNING: Changes to " + filename + " introduced linting errors!")
+
+        old_strings = past_lint_mapping[filename].splitlines(keepends=True)
+        new_strings = current_lint_mapping[filename].splitlines(keepends=True)
+        delta_generator = unified_diff(old_strings, new_strings,
+                                       fromfile=filename, tofile=filename,
+                                       n=0) # No lines of context
+        
+        diff_output.write("\n\n\n")
+        diff_output.writelines(delta_generator)
+
+    # We shouldn't even have a log file if no new errors were introduced.
+    if not any_errors_introduced:
+        try:
+            unlink(LOG_FILE)
+        except FileNotFoundError:
+            pass
+    else:
+        print("NOTICE: Check " + LOG_FILE + " for linting error details.")
+        with open(LOG_FILE, 'w') as f:
+            f.write(diff_output.getvalue())
+    diff_output.close()
+
+    # TODO: Accept/Reject based upon diffs matching or not.
 
 if __name__ == "__main__":
     main()
