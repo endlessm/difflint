9755290fab88d664a581e9cefd2920e708773ce2
Devin Ekins <devinj.ekins@gmail.com>
2015-04-17 09:57:12 -0600

    Added usage/help to linting script.
    A -h and --help option has been added to the linting script.
    This should be helpful when non-automated scripts intend to use
    the utility. Non-automated scripts have been known to call
    themselves "developers" in the past.

--- difflint/data/lint.sh
+++ difflint/data/lint.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+usage()
+{
+    cat <<EOF
+Usage:  $0 FILE [MODE]
+  or:   $0 -h
+Unless the -h option is given, this script performs linting on the given FILE
+according to its file extension. If multiple styles or languages are possible
+with a given extension, an optional MODE may be given to specify which linting
+tools and style rules will be used for that file.
+
+    Currently Supported MODEs:
+
+    gjs : GNOME JavaScript -- default
+    node : Node.js
+    web : browser JavaScript
+EOF
+}
+
 file_to_lint="$1"
 mode="$2"
 ret=0
 
 JSCS_REPORTER=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/jscs_terse_reporter.js"))'`
 JSHINT_REPORTER=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/jshint_terse_reporter.js"))'`
+
+if [ "$1" == "-h" -o "$1" == "--help" ]
+  then
+    usage
+    exit 0
+fi
 
 jscs "$file_to_lint" --reporter="$JSCS_REPORTER" || ret=$?
 
