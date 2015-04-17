a88a01b355f0866e8267c1df9fcd28895387847f
Devin Ekins <devinj.ekins@gmail.com>
2015-04-17 11:12:12 -0600

    Added checking functionality to lint.sh
    The linting script can now be used to determine if your
    system has the expected files in the expected locations.
    Of course, it does not make any guarantees that the contents
    of those files are appropriate.

--- difflint/data/lint.sh
+++ difflint/data/lint.sh
@@ -5,6 +5,7 @@ usage()
     cat <<EOF
 Usage:  $0 FILE [MODE]
   or:   $0 -h
+  or:   $0 -c
 Unless the -h option is given, this script performs linting on the given FILE
 according to its file extension. If multiple styles or languages are possible
 with a given extension, an optional MODE may be given to specify which linting
@@ -15,7 +16,35 @@ tools and style rules will be used for that file.
     gjs : GNOME JavaScript -- default
     node : Node.js
     web : browser JavaScript
+    
+    Options:
+
+    --help, -h : Display this usage information and exit.
+    --check, -c : Checks to see if all linting tools are where this script
+                  expects them to be. Displays a message indicating which
+                  tools were not found and returns 2. If all tools are found,
+                  instead displays a message indicating such and returns 0.
 EOF
+}
+
+check_executable()
+{
+  if [[ ! -f $(which "$1") ]]
+    then
+      echo "$1 not found."
+      return 2
+  fi
+  return 0
+}
+
+check_file()
+{
+  if [[ ! -f "$1" ]]
+    then
+      echo "$1 not found."
+      return 2
+  fi
+  return 0
 }
 
 file_to_lint="$1"
@@ -25,21 +54,37 @@ ret=0
 JSCS_REPORTER=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/jscs_terse_reporter.js"))'`
 JSHINT_REPORTER=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/jshint_terse_reporter.js"))'`
 
+NODE_JSHINTRC=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/node.jshintrc"))'`
+WEB_JSHINTRC=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/web.jshintrc"))'`
+
 if [ "$1" == "-h" -o "$1" == "--help" ]
   then
     usage
     exit 0
 fi
 
+if [ "$1" == "-c" -o "$1" == "--check" ]
+  then
+    check_executable "jscs" || ret=$?
+    check_executable "jshint" || ret=$?
+    check_file "$JSCS_REPORTER" || ret=$?
+    check_file "$JSHINT_REPORTER" || ret=$?
+    check_file "$NODE_JSHINTRC" || ret=$?
+    check_file "$WEB_JSHINTRC" || ret=$?
+    if [ "$ret" == "0" ]
+      then
+        echo "All configuration files and linting programs found."
+    fi
+    exit $ret
+fi
+
 jscs "$file_to_lint" --reporter="$JSCS_REPORTER" || ret=$?
 
 if [ "$mode" == "node" -o "$mode" == "nodejs" ]
   then
-    NODE_JSHINTRC=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/node.jshintrc"))'`
     jshint "$file_to_lint" --reporter="$JSHINT_REPORTER" --config="$NODE_JSHINTRC" || ret=$?
 elif [ "$mode" == "web" -o "$mode" == "webjs" ]
   then
-    WEB_JSHINTRC=`python3 -c 'print(__import__("pkg_resources").resource_filename("difflint", "data/web.jshintrc"))'`
     jshint "$file_to_lint" --reporter="$JSHINT_REPORTER" --config="$WEB_JSHINTRC" || ret=$?
 else
     jshint "$file_to_lint" --reporter="$JSHINT_REPORTER" || ret=$?
