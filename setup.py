74fd674c14de99a0a776b1bbedcc92ee2e422e6f
Philip Chimento <philip.chimento@gmail.com>
2015-08-21 15:53:38 -0700

    Add package metadata
    This will help to locate the package in various package directories.

--- setup.py
+++ setup.py
@@ -10,4 +10,12 @@ setup(name='difflint', version='0.0.0',
         'console_scripts': [
             'difflint = difflint:main',
         ],
-    })
+    },
+
+    # Metadata
+    author='Devin Ekins',
+    author_email='devinj.ekins@gmail.com',
+    description='Linter that will examine only new changes as you commit them',
+    license='GPLv2',
+    keywords='linter lint git commit hook node js',
+    url='https://github.com/endlessm/difflint')
